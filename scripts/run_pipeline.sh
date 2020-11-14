d9461cf1bc70b7f433ca6e9e0d4b30223b79dab7
Laurent Lam <laurent.lam@ensta-paristech.fr>
2020-11-14 18:52:45 +0100

    [init] add dataset fetch to pipeline bash script

--- scripts/run_pipeline.sh
+++ scripts/run_pipeline.sh
@@ -1,6 +1,11 @@
 echo "############### INSTALLING THE PACKAGE ###########"
-pip install -r requirements.txt
-pip install -e .
+pip3 install -r requirements.txt
+pip3 install -e .
+echo "###############          DONE          ###########"
+echo "############### FETCHING DATASET ###########"
+wget https://www.di.ens.fr/willow/teaching/recvis18orig/assignment3/bird_dataset.zip -P ./data
+unzip -q bird_dataset.zip -d ./data/
+rm ./data/bird_dataset.zip
 echo "###############          DONE          ###########"
 echo "###############  STARTING PIPELINE   ###########"
 python3 ./bird_image_classification_mva/scripts/pipeline_script.py
