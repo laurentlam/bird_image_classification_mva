e20929e6aa3afe20e481247b324f04f76257d254
Laurent Lam <laurent.lam@ensta-paristech.fr>
2020-11-22 12:34:11 +0100

    [bugfix] rm pip install from training/prediction scripts

--- scripts/run_prediction.sh
+++ scripts/run_prediction.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-echo "############### INSTALLING THE PACKAGE ###########"
-pip3 install -r requirements.txt
-pip3 install -e .
-echo "###############          DONE          ###########"
 echo "###############  STARTING PREDICTIONS   ###########"
 python3 ./bird_image_classification_mva/scripts/prediction_script.py
 echo "###############          DONE          ###########"
--- scripts/run_training.sh
+++ scripts/run_training.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-echo "############### INSTALLING THE PACKAGE ###########"
-pip3 install -r requirements.txt
-pip3 install -e .
-echo "###############          DONE          ###########"
 echo "############### STARTING PREPROCESSING ###########"
 python3 ./bird_image_classification_mva/scripts/preprocess_script.py
 echo "###############          DONE          ###########"
