d9a72527fb0a9c0f8a6c62f4304e8de44faa042c
Laurent Lam <laurent.lam@ensta-paristech.fr>
2020-11-22 12:24:55 +0100

    [bugfix] add tensorflow v2 upgrade script to MaskRCNN repo

--- scripts/run_extraction.sh
+++ scripts/run_extraction.sh
@@ -2,7 +2,9 @@
 
 echo "############### FETCHING EXTRACTION MODEL ###########"
 cd models
+pip3 uninstall mrcnn
 git clone https://github.com/matterport/Mask_RCNN.git
+tf_upgrade_v2 --intree Mask_RCNN --inplace --reportfile Mask_RCNN/report.txt
 echo "###############            DONE           ###########"
 cd Mask_RCNN
 echo "###############  EXTRACTION PIPELINE   ###########"
